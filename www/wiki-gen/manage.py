15ddc53e01b6454afb9b6bdba2b6e2f84917f8b7
Kevin Zheng <sunday0303@gmail.com>
2015-08-19 13:06:18 -0500

    Reorganize Flask functionality

--- www/wiki-gen/manage.py
+++ www/wiki-gen/manage.py
@@ -1,18 +1,46 @@
+import os
 from flask.ext.script import Manager
-from wiki_gen import app, init_all_db
+from wiki_gen import app, connect_seeds_db
+
+
+def init_seeds_db():
+    '''Initialize the seeds logging database'''
+    if os.path.isfile(app.config['DATABASE_SEEDS']):
+        raise NameError('{} already exists'.format(app.config['DATABASE_SEEDS']))
+        return 1
+    db = connect_seeds_db()
+    with open('schemas/seeds.sql') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
 
 manager = Manager(app)
-
 
 @manager.command
 def initdb():
     '''Create all database tables'''
-    init_all_db()
+    try:
+        init_seeds_db()
+    except NameError as e:
+        print e.message
+        return 1
+    except:
+        print 'There was an error creating the database'
+        return 1
+    print 'Database created'
+    return 0
 
 @manager.command
 def runglobalserver():
     '''Run the server on 0.0.0.0'''
     app.run(host='0.0.0.0')
+
+@manager.command
+def runservernodebug():
+    '''Run the server on localhost without debug'''
+    app.run(debug=False)
 
 if __name__ == '__main__':
     manager.run()
--- www/wiki-gen/wiki_gen.py
+++ www/wiki-gen/wiki_gen.py
@@ -9,37 +9,23 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-### Database handling/utilities
+### Database connections
 def connect_seeds_db():
     '''Connect to the seeds database'''
-    return sqlite3.connect(app.config['DATABASE_SEEDS'])
+    if not hasattr(g, 'db_seeds'):
+        g.db_seeds = sqlite3.connect(app.config['DATABASE_SEEDS'])
+    return g.db_seeds
 
 def connect_grams_db():
     '''Connect to the grams database'''
-    return sqlite3.connect(app.config['DATABASE_GRAMS'])
-
-def init_seeds_db():
-    '''Initialize the seeds logging database'''
-    if os.path.isfile(app.config['DATABASE_SEEDS']):
-        raise Exception('{} already exists'.format(app.config['DATABASE_SEEDS']))
-        return
-    db = connect_seeds_db()
-    with open('schemas/seeds.sql') as f:
-        db.cursor().executescript(f.read())
-    db.close()
-
-def init_all_db():
-    '''Initialize all databases'''
-    init_seeds_db()
+    if not hasattr(g, 'db_grams'):
+        g.db_grams= sqlite3.connect(app.config['DATABASE_GRAMS'])
+    return g.db_grams
 
 
 ### Request hooks
-@app.before_request
-def before_request():
-    pass
-
 @app.teardown_request
-def teardown_request(exception):
+def close_dbs(exception):
     '''Close database connections'''
     for db_key in ['db_seeds', 'db_grams']:
         db = getattr(g, db_key, None) 
@@ -50,15 +36,20 @@ def teardown_request(exception):
 ### Routes
 @app.route('/')
 def home():
+    seeds_db_conn = connect_seeds_db()
     return 'Hello!' 
 
 @app.route('/gen')
 def generate():
-    seed_1 = request.args.get('seed1')
-    seed_2 = request.args.get('seed2')
-    seed_3 = request.args.get('seed3')
-    grams_db_conn = connect_grams_db()
-    grams_db_cur = grams_db_conn.cursor()
-    gen = MarkovGenerator(grams_db_cur, seed_1, seed_2, seed_3)
-    result = gen.generate()
-    return result
+    raw_seed = request.args.get('seed')
+    return raw_seed
+
+
+### Exception handlers
+@app.errorhandler(404)
+def not_found(exception):
+    return 'Page not found', 404
+
+@app.errorhandler(500)
+def server_error(exception):
+    return 'Server error', 500
